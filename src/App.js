9fc6eb16c8a06aa05787f93b4df3933fe7904180
Manasvi Arora <manasviarora28@gmail.com>
2024-01-16 16:46:59 +0530

    Home page and PostPage set

--- src/App.js
+++ src/App.js
@@ -11,21 +11,57 @@ import PostPage from './PostPage';
 import About from './About';
 import Missing from './Missing';
 
+
 function App() {
+  const [search,setSearch] = useState('');
+  const [posts,setPosts] = useState([{
+    id: 1,
+    title: "My First Post",
+    datetime: "July 01, 2021 11:17:36 AM",
+    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+  },
+  {
+    id: 2,
+    title: "My 2nd Post",
+    datetime: "July 01, 2021 11:17:36 AM",
+    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+  },
+  {
+    id: 3,
+    title: "My 3rd Post",
+    datetime: "July 01, 2021 11:17:36 AM",
+    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+  },
+  {
+    id: 4,
+    title: "My Fourth Post",
+    datetime: "July 01, 2021 11:17:36 AM",
+    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+  }
+])
+
+
+  
+  const [searchResults,setSearchResults] = useState('');
+  
+
   return (
     <>
-    <Header/>
-    <Nav/>
-    <BrowserRouter>
+  <BrowserRouter>
+    <Header title = 'React JS Blog'/>
+    <Nav 
+      search = {search}
+      setSearch={setSearch}  
+    />
       <Routes>
-        <Route path ='/' element ={<Home/>}></Route>
+        <Route path ='/' element ={<Home posts={posts}/>}></Route>
         <Route path ='/post' element = {<NewPost/>}></Route>
-        <Route path ='/post/:id' element ={<PostPage/>}></Route>
+        <Route path ='/post/:id' element ={<PostPage posts={posts} setPosts={setPosts}/>}></Route>
         <Route path ='/about' element = {<About/>}></Route>
         <Route path = '*' element = {<Missing/>}></Route>
       </Routes>
-    </BrowserRouter>
     <Footer/>
+    </BrowserRouter>
     </>
   );
 }
--- src/PostPage.js
+++ src/PostPage.js
@@ -1,9 +1,41 @@
 import React from 'react'
+import { useParams, Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
-const PostPage = () => {
+const PostPage = ({posts,setPosts}) => {
+  const { id } = useParams(); /* gets the id from the localHost link */
+  const post = posts.find(post => (post.id).toString() === id );
+  const navigate = useNavigate();
+  const handleDelete = (id) => {
+    const postsList = posts.filter(post => post.id !==id);
+    setPosts(postsList);
+    navigate('/');
+    
+  }
+
   return (
-    <main>
-      <h1>PostPage</h1>
+    <main className='PostPage'>
+      <article>
+        {post && 
+          <>
+            <h2>{post.title}</h2>
+            <p className='postDate'>{post.datetime}</p>
+            <p className='postBody'>{post.body}</p>
+            <button onClick = {() => handleDelete(post.id)}>
+              Delete Post
+            </button>
+          </>
+        }
+        {!post &&
+          <>
+            <h2>Post Not Found</h2>
+            <p>Well that's disappointing</p>
+            <p>
+              <Link to ='/'>Visit Our Homepage</Link>
+            </p>
+          </>
+        }
+      </article>
     </main>
   )
 }
